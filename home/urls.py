afc517efd24eb7089d26f8fbb99c17bb4c06159e
shubham bhadale <shubhambhadale474@gmail.com>
2023-06-16 06:51:06 +0530

    added file upload and copy function

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import FilesAPIView
+from . import views
 
 urlpatterns = [
-    path('files/', FilesAPIView.as_view())
+    path('files/', FilesAPIView.as_view()),
+    path('', views.home)
 ]
--- home/views.py
+++ home/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import FilesSerializer
+from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
 
 
 class FilesAPIView(APIView):
 
     def post(self, request):
         data = request.data
+        print(data)
         serializer = FilesSerializer(data=data)
 
         if serializer.is_valid():
